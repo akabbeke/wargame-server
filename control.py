820688d7fb6925c0ffccd4983c4277046ff0f506
adam <akabbeke@gmail.com>
2020-06-13 19:53:07 -0700

    Add python based rcon

--- control.py
+++ control.py
@@ -5,8 +5,8 @@
     Wargame Server Control Script
     Original Author: DesertEagle
     Modified By: kissinger
-    
-    Requirements: 
+
+    Requirements:
         pip3 install python-geoip-python3 python-geoip-geolite2 scapy
 
     Type-checking:
@@ -14,26 +14,30 @@
 
 """
 
-from geoip import geolite2 # type: ignore
-
-from typing import Tuple, Pattern, List, Iterable, Optional, Callable, Any, Dict, Match, IO
 import argparse
-import re
-import os
-import select, sys
-from time import sleep
-import time
-from subprocess import call
-from enum import Enum
-from random import random
-from math import floor
-from threading import Thread
 import collections
+import os
 import queue
+import re
+import select
+import sys
+import time
+from enum import Enum
+from math import floor
+from random import random
+from subprocess import call
+from threading import Thread
+from time import sleep
+from typing import (IO, Any, Callable, Dict, Iterable, List, Match, Optional,
+                    Pattern, Tuple)
+
+from geoip import geolite2  # type: ignore
+from py_rcon import PyRcon
+
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 #================================================================================#
-# environment parameters 
+# environment parameters
 #================================================================================#
 DEFAULT_RCON_PORT = '10842'
 DEFAULT_RCON_PATH = "/srv/wargame/wargame3_server/mcrcon/mcrcon"
@@ -43,7 +47,7 @@ DEFAULT_CHAT_PATH = "chat.txt"
 SERVER_LOG_PATH = "serverlog.txt"
 
 #================================================================================#
-# your specific lobby's parameters 
+# your specific lobby's parameters
 #================================================================================#
 MIN_PLAYER_LEVEL = 5
 LOBBY_RULES = '[EXPERIMENTAL, type "command" for more commands] server rules: strictly no teamkilling (even in self-defense); mark starting zones with flare or chat; minimum player level: ' + str(MIN_PLAYER_LEVEL)
@@ -131,7 +135,7 @@ class Player:
 
     def get_port(self) -> int:
         return self._port
-    
+
     def get_side(self) -> Side:
         return self._side
 
@@ -174,7 +178,7 @@ class Player:
         else:
             side = Side.Bluefor
         Rcon.execute("setpvar " + self._id + " PlayerAlliance " + str(side))
-    
+
     def change_side(self, side: int) -> None:
         """Forcibly change player's side"""
         Rcon.execute("setpvar " + self._id + " PlayerAlliance " + str(side))
@@ -208,10 +212,10 @@ class Server:
 
         # TODO: this is not working yet in the patched binary, so we're just always setting it to the client id. remove this line once it works.
         client_id_hex = 0xffffffff
-            
+
         # strip the 0x prefix on the hex client id
         Rcon.execute("chat " + ('%08x' % client_id_hex) + " " + message)
-        
+
     @classmethod
     def change_map(cls, mapname: str) -> None:
         Rcon.execute("setsvar Map " + mapname)
@@ -230,7 +234,7 @@ class Server:
     @classmethod
     def change_min_players_to_start(cls, number: int) -> None:
         Rcon.execute("setsvar NbMinPlayer " + str(number))
-        
+
     @classmethod
     def change_time_limit(cls, number: int) -> None:
         Rcon.execute("setsvar TimeLimit " + str(number))
@@ -254,7 +258,7 @@ class Server:
     @classmethod
     def change_date_constraint(cls, number: int) -> None:
         Rcon.execute("setsvar DateConstraint " + str(number))
-    
+
 class Rcon:
     """ Rcon connection settings """
     rcon_path: str = DEFAULT_RCON_PATH
@@ -264,19 +268,23 @@ class Rcon:
 
     @classmethod
     def execute(cls, command: str) -> None:
-        """Execute rcon command, incapsulating details"""
-        execution_string = cls.rcon_path + ' -H ' + cls.rcon_host + ' -P ' + cls.rcon_port + \
-            ' -p ' + cls.rcon_password + ' "' + command + '"'
-        call(execution_string, shell=True)
+        client = PyRcon()
+        client.connect(
+            cls.rcon_host,
+            cls.rcon_port,
+            cls.rcon_password,
+        )
+        return client.command(command)
+
 
 class Game:
     """Main class, containing game process manipulation"""
-    last_message: Optional[str] = None    
-    
+    last_message: Optional[str] = None
+
     # -------------------------------------------
     # User event handlers
     # -------------------------------------------
-    
+
     def on_player_connect(self, playerid: str) -> None:
         pass #Server.send_message(LOBBY_RULES, playerid)
 
@@ -289,7 +297,7 @@ class Game:
         if not from_player:
             print('error: player not found for id: ' + client_id)
             return
-                    
+
         print('[' + str(from_player.get_id()) + ':' + from_player.get_name() + ']: ' + msg)
         if msg == 'rules':
             print('sending rules')
@@ -308,7 +316,7 @@ class Game:
             s = []
             for player in self.players.values():
                 match = geolite2.lookup(player.get_ip())
-                if match:                    
+                if match:
                     s.append(player.get_name() + ': ' + match.country)
             Server.send_message(', '.join(s))
 
@@ -369,7 +377,7 @@ class Game:
             if player.get_ip() == ip and player.get_port() == port:
                 return player
         return None
-            
+
     def handle_rotate_request(self, from_player: Player) -> None:
         from_player.votes['rotate'][1] = True
         nvotes = self.count_votes('rotate', 1, same_team=False)
@@ -393,7 +401,7 @@ class Game:
             # self.assign_decks()
             for player in self.players.values():
                 player.votes['year'] = {}
-                
+
     def handle_kick_request(self, msg: str, from_player: Player) -> None:
         parts = msg[len('kick '):]
         kickable_player = self.find_player_id_by_name(parts)
@@ -408,7 +416,7 @@ class Game:
                         del player.votes['kick'][kickable_player.get_id()]
         else:
             Server.send_message('player ' + parts + ' not found')
-    
+
     def assign_decks(self) -> None:
         """Forcing specific deck usage"""
         for playerID, player in self.players.items():
@@ -446,7 +454,7 @@ class Game:
     def _on_player_connect(self, match_obj: Match[str]) -> None:
         playerid = match_obj.group(1)
         player_ip = match_obj.group(2)
-        player_port = match_obj.group(3) 
+        player_port = match_obj.group(3)
         # Creating player data structure if not present
         if not (playerid in self.players):
             self.players[playerid] = Player(playerid, player_ip, int(player_port))
@@ -540,7 +548,7 @@ class Game:
 
         if not self.infoRun:
             self.on_switch_to_deployment()
-            
+
     # ---------------------------------------------
     # Event handlers registration
     # ---------------------------------------------
@@ -554,7 +562,7 @@ class Game:
         self.register_event('Client ([0-9]+) variable PlayerName set to "(.*)"', self._on_player_name_change)
         self.register_event('Disconnecting client ([0-9]+)', self._on_player_disconnect)
         self.register_event('Entering in loading phase state', self._on_switch_to_game)
-        self.register_event('Entering in deploiement phase state', self._on_switch_to_deployment)        
+        self.register_event('Entering in deploiement phase state', self._on_switch_to_deployment)
         self.register_event('Entering in debriephing phase state', self._on_switch_to_debriefing)
         self.register_event('Entering in matchmaking state', self._on_switch_to_lobby)
 
@@ -654,17 +662,17 @@ Server.change_victory_cond(1)
             return 0
         return float(acc) / float(count)
 
-    def message_average_team_info(self, force: bool=False) -> None: 
+    def message_average_team_info(self, force: bool=False) -> None:
         blue = 'avg blue: {:.2f}'.format(self.average_player_level(self.players.values(), Side.Bluefor))
         red = 'avg red: {:.2f}'.format(self.average_player_level(self.players.values(), Side.Redfor))
         msg = blue + " - " + red
         if msg != self.last_message or force:
             Server.send_message(msg)
         self.last_message = msg
-    
+
     def dump_state(self) -> None:
         #print(chr(27) + "[2J")
-        
+
         print("We have {} players:".format(len(self.players)))
         for player in sorted(self.players.values(), key=lambda x: str(x.get_side())):
             print('[{}] {}:\t{}\t{}'.format(str(player.get_side()), str(player.get_level()), player.get_id(), player.get_name()))
@@ -699,7 +707,7 @@ def main(args: argparse.Namespace) -> None:
     Rcon.rcon_password = args.rcon_password
     Rcon.rcon_port = args.rcon_port
     Rcon.rcon_path = args.rcon_path
-    
+
     sniff_thread = Thread(target = parse_chat)
     sniff_thread.start()
     update_thread = Thread(target = update_game)
@@ -715,7 +723,7 @@ if __name__ == "__main__":
     parser.add_argument("--rcon_path", help="rcon path", default=DEFAULT_RCON_PATH)
     parser.add_argument("--rcon_password", help="rcon password", default=DEFAULT_RCON_PASSWORD)
     parser.add_argument("--chat_path", help="path to the server chat log", default=DEFAULT_CHAT_PATH)
-    args = parser.parse_args() 
+    args = parser.parse_args()
     print('expecting to see server logs in :' + SERVER_LOG_PATH)
     print('expecting to see chat logs in :' + DEFAULT_CHAT_PATH)
     main(args)
